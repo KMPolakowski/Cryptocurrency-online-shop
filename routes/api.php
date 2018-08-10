1ad75de1a8723931656941947990935963e152ff
KM Polakowski <kamilpol00@gmail.com>
2018-08-10 14:50:53 +0200

    10.08

--- routes/api.php
+++ routes/api.php
@@ -17,23 +17,26 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('all_transactions', 'ApiController@getAllTransactions');
 
+//Transactions of certain user (api_token)
 Route::get('my_transactions', 'ApiController@getMyTransactions');
 
-
+// Data from CoinMarketCap.com, free access
 Route::get('dashboard/{page}/{selectedCurrency}', 'ApiController@getDashboard');
-
 Route::get('listings', 'ApiController@getListings');
-Route::get('internal_listings', 'ApiController@getInternalListings');
-Route::get('coin_price/{id}', 'ApiController@getCoinPrice');
 Route::get('coin/{id}', 'ApiController@getCoin');
 
+//Data that is cached in redis cluster, free access
+Route::get('internal_listings', 'ApiController@getInternalListings');
+Route::get('coin_price/{id}', 'ApiController@getCoinPrice');
 
 
+
+
+// Access allowed only for "super user" with id 1 (api_token)
 Route::get('update_prices', 'ApiController@updatePrices');
 Route::get('update_listings', 'ApiController@updateListings');
-
+Route::get('all_transactions', 'ApiController@getAllTransactions');
 
 
 
